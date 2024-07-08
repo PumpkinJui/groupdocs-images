7355bf75a9066e64f110e5ed0a7a552404234830
PumpkinJui <ZXN070320@163.com>
2024-07-09 02:24:02 +0800

    Add compress.sh

---
+++ compress.sh
@@ -0,0 +1,18 @@
+# by 通义千问
+
+# 指定要处理的目录
+input_dir="assets/"
+
+# 使用find命令找到目录及其子目录中的所有文件，并存储到一个数组中
+mapfile -t files < <(find "$input_dir" -type f)
+
+# 遍历文件数组
+for file in "${files[@]}"; do
+    # 构建输出文件路径，添加'_compressed'后缀
+    output_file="../groupdocs/${file%.*}_compressed.jpg"
+
+    # 使用FFmpeg进行压缩
+    ffmpeg -i "$file" -qscale 15 -preset medium "$output_file"
+done
+
+echo "压缩完成。"
