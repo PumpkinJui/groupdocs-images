2ee3e875d29b5f076fbcb125665c3bcea492a667
PumpkinJui <ZXN070320@163.com>
2024-08-26 23:57:31 +0800

    BP240825

--- compress_pending.sh
+++ compress_pending.sh
@@ -1,7 +1,7 @@
 # by 通义千问
 
 # 指定要处理的目录
-input_dir="assets/"
+input_dir="pending/"
 
 # 使用 find 命令找到目录及其子目录中的所有文件，并存储到一个数组中
 mapfile -t files < <(find "$input_dir" -type f)
@@ -10,7 +10,7 @@ mapfile -t files < <(find "$input_dir" -type f)
 for file in "${files[@]}"; do
     # 构建输出文件路径，添加 '_compressed' 后缀
     base_name=$(basename "$file")
-    output_file="../groupdocs/assets/${base_name%.*}_compressed.jpg"
+    output_file="done/${base_name%.*}.jpg"
 
     # 使用 FFmpeg 进行压缩；qscale=15；crf=23
     ffmpeg -i "$file" -qscale 15 -preset medium "$output_file"
